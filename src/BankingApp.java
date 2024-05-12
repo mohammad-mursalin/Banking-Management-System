325781e3b84ff672ed8ca55040c5bfb3085c47d6
mohammad_mursalin <mursalinlamon@gmail.com>
2024-05-12 11:39:08 +0600

    functions added

---
+++ src/BankingApp.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class BankingApp {
+
+    private static String url = "jdbc:postgresql://localhost/postgresdemo";
+    private static String username = "postgres";
+    private static String password = "mursalin@postgres"; 
+
+    public static void main(String[] args) {
+
+        try {
+            
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Scanner scanner = new Scanner(System.in);
+
+            User user = new User(connection, scanner);
+            
+            while(true) {
+
+                System.out.println();
+                System.out.println("Banking Management App");
+                System.out.println();
+                System.out.println("1. Register");
+                System.out.println("2. Login");
+                System.out.println();
+                System.out.println("Chose your option : ");
+                int choice = scanner.nextInt();
+
+                switch(choice) {
+
+                    case 1 -> user.register();
+
+                    case 2 -> user.login();
+                }
+            }
+
+        } catch (SQLException e) {
+            
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
--- src/User.java
+++ src/User.java
@@ -1,2 +1,69 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class User {
+
+    Scanner scanner;
+    Connection connection;
+
+    User(Connection connection, Scanner scanner) {
+
+        this.connection = connection;
+        this.scanner = scanner;
+    }
+
+    public void register() {
+
+        System.out.println();
+        System.out.println("Username : ");
+        String username = scanner.nextLine();
+        System.out.println("Email : ");
+        String email = scanner.nextLine();
+        System.out.println("Password : ");
+        String password = scanner.nextLine();
+
+        if(!registration_exist()) {
+
+            try {
+
+                String query = "insert into users values(?,?,?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+    
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(2, email);
+                preparedStatement.setString(3, password);
+    
+                int affectedRow = preparedStatement.executeUpdate();
+    
+                if(affectedRow > 0) {
+    
+                    System.out.println("User registration successful");
+                }
+                else {
+    
+                    System.out.println("User registration failed");
+                }
+    
+            } catch (SQLException e) {
+                
+                e.printStackTrace();
+            }
+        }
+        else {
+
+            System.out.println("Registration for this user already exist");
+        }
+    }
+
+    public void login() {
+
+
+    }
+
+    public Boolean registration_exist() {
+
+
+    }
 }
