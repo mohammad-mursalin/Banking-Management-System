cb14fc766fe3817e9fa3cb8114a05484767fc32c
mohammad_mursalin <mursalinlamon@gmail.com>
2024-05-18 11:13:35 +0600

    transfer amount function added

--- src/AccountManager.java
+++ src/AccountManager.java
@@ -175,7 +175,7 @@ public class AccountManager {
         connection.setAutoCommit(true);
     }
 
-    public void transferMoney(long senderAccountNumber) {
+    public void transferMoney(long senderAccountNumber) throws SQLException {
 
         try {
 
@@ -184,12 +184,70 @@ public class AccountManager {
             System.out.println();
             System.out.print("Enter reciever account number : ");
             long recieverAccountNumber = scanner.nextLong();
+            System.out.print("Enter amount : ");
+            double transferBalance = scanner.nextDouble();
             System.out.print("Enter security pin : ");
             String securityPin = scanner.nextLine();
             
             if(senderAccountNumber != 0 && recieverAccountNumber != 0) {
 
+                String query = "select balance from accounts where account_number = ? and security_pin = ?";
 
+                PreparedStatement preparedStatement = connection.prepareStatement(query);
+                preparedStatement.setLong(1, senderAccountNumber);
+                preparedStatement.setString(2, securityPin);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                if(resultSet.next()) {
+
+                    double senderBalance = resultSet.getDouble("balance");
+
+                    if(transferBalance <= senderBalance) {
+
+                        String debitQuery = "update accounts set balance = - ? where account_number = ? and security_pin = ?";
+                        PreparedStatement debitPreparedStatement = connection.prepareStatement(debitQuery);
+                        debitPreparedStatement.setDouble(1, transferBalance);
+                        debitPreparedStatement.setLong(2, senderAccountNumber);
+                        debitPreparedStatement.setString(3, securityPin);
+
+                        int debitAccountAffectedRow = debitPreparedStatement.executeUpdate();
+
+                        String creditQuery = "update accounts set balance = + ? where account_number = ? ";
+                        PreparedStatement creditPreparedStatement = connection.prepareStatement(creditQuery);
+                        debitPreparedStatement.setDouble(1, transferBalance);
+                        debitPreparedStatement.setLong(2, recieverAccountNumber);
+
+                        int creditAccountAffectedRow = creditPreparedStatement.executeUpdate();
+
+                        if( debitAccountAffectedRow > 0 && creditAccountAffectedRow > 0) {
+
+                            System.out.println();
+                            System.out.println("Balance " +transferBalance+" transfered successfully");
+
+                            connection.commit();
+                            connection.setAutoCommit(true);
+                        }
+                        else {
+
+                            System.out.println();
+                            System.out.println("Balance transfer unsuccessful");
+
+                            connection.rollback();
+                            connection.setAutoCommit(true);
+                        }
+                    }
+                    else {
+
+                        System.out.println();
+                        System.out.println("Insufficient balance");
+                    }
+                }
+                else {
+
+                    System.out.println();
+                    System.out.println("Invalid security pin");
+                }
             }
             else {
 
@@ -198,8 +256,10 @@ public class AccountManager {
             }
         } catch (SQLException e) {
             
-
+            e.printStackTrace();
         }
+
+        connection.setAutoCommit(true);
     }
 
     public void checkBalance(long accountNumber) {
